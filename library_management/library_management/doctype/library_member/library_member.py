d2b4b4be8b0822f97a4096f965869461d60c143a
SidaganaUsha <usha@cruxedge.com>
2025-05-19 12:09:11 +0530

    Created Library member Doctype.

---
+++ library_management/library_management/doctype/library_member/library_member.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2025, Usha Sidagana and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class LibraryMember(Document):
+	def before_save(self):
+		# this method will run every time a document is saved
+		self.full_name = f'{self.first_name} {self.last_name or ""}'
